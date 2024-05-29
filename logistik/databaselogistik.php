874567a24b64f3a369de952232231caf7df7b74c
Viktor Jancic <148555092+Viktor-j2021@users.noreply.github.com>
2024-05-29 18:42:16 +0200

    Update databaselogistik.php

--- logistik/databaselogistik.php
+++ logistik/databaselogistik.php
@@ -3,7 +3,7 @@
 
 if (isset($_POST['submit'])) {
 
-    require_once __DIR__ . '/Databasetabelle.php';
+    require_once __DIR__ . './../logistik/Databasetabelle.php';
 
     Databasetabelle::speichert($name, $email, $telefon, $message);
 }
